2a247ebbd5de389b0bb5715284bb43410e580da9
Tezan Sahu <tezansahu@gmail.com>
2020-07-06 12:58:47 +0530

    added contact & social links

--- js/contact.js
+++ js/contact.js
@@ -35,23 +35,23 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
+                    required: "You must enter your name!",
+                    minlength: "Your name must consist of at least 2 characters"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
+                    required: "You must have a subject for the message!",
+                    minlength: "Your subject must consist of at least 4 characters"
                 },
                 number: {
                     required: "come on, you have a number, don't you?",
                     minlength: "your Number must consist of at least 5 characters"
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Can't send message without an email ID"
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "You have to write something to send this form.",
+                    minlength: "That's all? Really?"
                 }
             },
             submitHandler: function(form) {
