3e0740dc597b95d2a6f13f49c077aed40682ea69
Tezan Sahu <tezansahu@gmail.com>
2020-12-25 11:15:22 +0530

    updated projects

--- js/projects.js
+++ js/projects.js
@@ -1,14 +1,18 @@
 let cur_proj_lev = 0
 let max_proj_lev = 1
 
+
 function showMore(){
-    if(cur_proj_lev == 0){
-        document.getElementById("projects-more-1").style.display = "block";
-    }
+    
+    // hidden_projects.style.display = "inline-block"
+    
+    // if(cur_proj_lev == 0){
+    //     document.getElementById("projects-more-1").style.display = "block";
+    // }
 
-    cur_proj_lev += 1;
+    // cur_proj_lev += 1;
 
-    if(cur_proj_lev == max_proj_lev){
-        document.getElementById("show-more").style.display = "none";
-    }
+    // if(cur_proj_lev == max_proj_lev){
+    //     document.getElementById("show-more").style.display = "none";
+    // }
 }
